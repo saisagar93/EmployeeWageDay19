9b203cf3c66196f393ecd8ac3b704eb75c8f216e
saisagar93 <sscreator225@gmail.com>
2020-12-25 12:36:50 +0530

    Add:-code to Check Employee Attendence

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,5 +3,16 @@ public class EmployeeWageComputation
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage computation");
-        }
+                int present=1;
+		int empCheck=(int)Math.floor(Math.random()*10)%2;
+		System.out.println("Random Value: "+empCheck);
+		if(empCheck==present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
 }
