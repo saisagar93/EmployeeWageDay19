2385dfc8660e99b3ec99ad1bd080bd009d8a3a7e
saisagar93 <sscreator225@gmail.com>
2020-12-25 12:55:21 +0530

    Add:- Code Solving using Switch case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,29 +1,33 @@
-public class EmployeeWageComputation						//Main class					
+public class EmployeeWageComputation						//Main class
 {
+	//Constants
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+
 	public static void main(String[] args)					//Main method
 	{
 
 		System.out.println("Welcome to employee wage computation");
-		int fullTime=1;
-		int partTime=2;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;		//To generate a random valueof 0 to 2
+		//variables
 		int empHrs=0;
-		int empRatePerHr=20;
 		int salary=0;
-
-		if(empCheck==fullTime)						//Checking if employee is fullTime
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch(empCheck)						//switch case for employee fulltime,parttime and absent 
 		{
+			case FULL_TIME:
 			empHrs=8;
-		}
-		else if(empCheck==partTime)					//Checking if Employee is part time
-		{
+			break;
+
+			case PART_TIME:
 			empHrs=4;
-		}
-		else								//Checking if employee is absent
-		{
+			break;
+
+			default:
 			empHrs=0;
 		}
-		salary=empHrs*empRatePerHr;					//salary of an employee
-		System.out.println("Employee Wage: "+salary);			//To print daily salary of an employee 
+		salary=empHrs*EMP_RATE_PER_HR;					//salary of an employee
+                System.out.println("Employee Wage: "+salary);
 	}
 }
