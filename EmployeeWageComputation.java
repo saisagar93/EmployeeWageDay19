71478dcb9570f58f4c1af7a0a85a15055c00ac39
saisagar93 <sscreator225@gmail.com>
2020-12-25 12:48:18 +0530

    Add:-Code part time Employee & Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,21 +1,27 @@
-public class EmployeeWageComputation						//Main class
+public class EmployeeWageComputation						//Main class					
 {
 	public static void main(String[] args)					//Main method
 	{
 
 		System.out.println("Welcome to employee wage computation");
 		int fullTime=1;
-		int empCheck=(int)Math.floor(Math.random()*10)%2;		//To generate a random valueof 0 or 1
+		int partTime=2;
+		int empCheck=(int)Math.floor(Math.random()*10)%3;		//To generate a random valueof 0 to 2
 		int empHrs=0;
 		int empRatePerHr=20;
 		int salary=0;
-		if(empCheck==fullTime)						//Checking for employee is fullTime or absent 
+
+		if(empCheck==fullTime)						//Checking if employee is fullTime
 		{
 			empHrs=8;
 		}
-		else
+		else if(empCheck==partTime)					//Checking if Employee is part time
 		{
-			salary=0;
+			empHrs=4;
+		}
+		else								//Checking if employee is absent
+		{
+			empHrs=0;
 		}
 		salary=empHrs*empRatePerHr;					//salary of an employee
 		System.out.println("Employee Wage: "+salary);			//To print daily salary of an employee 
