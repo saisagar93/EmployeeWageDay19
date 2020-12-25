192ba8cb4e70981bb228f73ed017ec643abde223
saisagar93 <sscreator225@gmail.com>
2020-12-25 12:42:55 +0530

    Add:-Code Calculate Daily Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,23 @@
-public class EmployeeWageComputation
+public class EmployeeWageComputation						//Main class
 {
-	public static void main(String[] args)
+	public static void main(String[] args)					//Main method
 	{
+
 		System.out.println("Welcome to employee wage computation");
-                int present=1;
-		int empCheck=(int)Math.floor(Math.random()*10)%2;
-		System.out.println("Random Value: "+empCheck);
-		if(empCheck==present)
+		int fullTime=1;
+		int empCheck=(int)Math.floor(Math.random()*10)%2;		//To generate a random valueof 0 or 1
+		int empHrs=0;
+		int empRatePerHr=20;
+		int salary=0;
+		if(empCheck==fullTime)						//Checking for employee is fullTime or absent 
 		{
-			System.out.println("Employee is present");
+			empHrs=8;
 		}
 		else
 		{
-			System.out.println("Employee is absent");
+			salary=0;
 		}
+		salary=empHrs*empRatePerHr;					//salary of an employee
+		System.out.println("Employee Wage: "+salary);			//To print daily salary of an employee 
 	}
 }
